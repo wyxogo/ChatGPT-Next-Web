c6cf4412a2689878c98d664d6d27277974914d05
Yasin <61509470+wyxogo@users.noreply.github.com>
2024-01-07 00:03:32 +0800

    Restore: OWNER

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,4 @@
-export const OWNER = "wyxogo";
-// export const OWNER = "Yidadaa";
+export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
